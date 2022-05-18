f3aa8d05a6d24335565cd5e258c4bae7a56257d3
Amine BOUNIZEL <al1069285@tdf.hubtotal.net>
2022-05-18 11:06:57 +0200

    260

--- scripts/run.py
+++ scripts/run.py
@@ -74,7 +74,7 @@ def main():
             with open(json_dir_path + '/' + file_name + '.json', "w") as f:
                 json.dump(data, f, indent=4)
              #Connection
-            with open(output_path, "w") as f:
+            with open(json_dir_path + '/' + 'Connections' + '.json', "w") as f:
                 json.dump(data0, f, indent=4)
 
             print('Pretty Printed {}'.format(file))
