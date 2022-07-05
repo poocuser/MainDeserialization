9db6e59e1f1c7c0203897cdeccc0724b06982af6
Amine BOUNIZEL <poocuser@6ysf6f.onmicrosoft.com>
2022-07-05 16:25:44 +0200

    initxcwvsw

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -488,7 +488,77 @@ Function CiBuild {
         if ($p2.ExitCode -ne 0) {
 			Write-Error "$indention Failed to deploy .bim file!"
 		}
+#---------------------------------------------------------SPLIT PBIX--------------------------------------------------------------------------------
+#Error check function
+function IsFileLocked([string]$filePath){
+    Rename-Item $filePath $filePath -ErrorVariable errs -ErrorAction SilentlyContinue
+    return ($errs.Count -ne 0)
+}
 
+
+#Function to Modify files
+Function Modify-PBIX([string]$inputpath, [string[]]$filestoremove){
+
+    #Make temp folder
+    $temppth = $env:TEMP  + "\PBI TEMP"
+    If(!(test-path $temppth))
+    {New-Item -ItemType Directory -Force -Path $temppth}
+
+    #Unpackage pbix
+    $zipfile = ($inputpath).Substring(0,($inputpath).Length-4) + "zip"
+    Rename-Item -Path $inputpath -NewName  $zipfile
+              
+    #Initialise object
+    $ShellApp = New-Object -COM 'Shell.Application'
+    $InputZipFile = $ShellApp.NameSpace( $zipfile )
+
+    #Move files to temp
+    foreach ($fn in $filestoremove){ 
+       $InputZipFile.Items() | ? {  ($_.Name -eq $fn) }  | % {
+       $ShellApp.NameSpace($temppth).MoveHere($_)   }  
+    }
+    
+    #Delete temp
+    Remove-Item ($temppth) -Recurse
+    
+    #Repackage 
+    Rename-Item -Path $zipfile -NewName $inputpath  
+}
+
+#Choose file
+$pathn = $pbix_file.FullName
+#try {$pathn = Get-FileName}
+#catch { "Incompatible File" }
+
+
+#Check for errors
+If([string]::IsNullOrEmpty($pathn )){            
+    exit } 
+
+elseif ( IsFileLocked($pathn) ){
+    exit } 
+
+#Run Script
+else{    
+
+    #set variables
+    $modelfiles   = @( 'SecurityBindings', 'Report')
+    $reportfiles   = @('Connections','DataModel',  'SecurityBindings')
+    
+    #Copy files
+    $pathf = Get-ChildItem $pathn
+    $reportname = [io.path]::GetFileNameWithoutExtension($pathn)
+    $model = ($pathf).toString().Replace('.pbix', '_model.pbix')
+    $report = ($pathf).toString().Replace('.pbix', '_report.pbix')    
+    Copy-Item $pathn -Destination $model
+    Copy-Item $pathn -Destination $report
+
+    #modify files
+    Modify-PBIX $model $modelfiles
+    Modify-PBIX $report $reportfiles
+    
+}
+#---------------------------------------------------------SPLIT PBIX-END-------------------------------------------------------------------------------
         #Publish the report
         Write-Information "Processing  $($pbix_file.FullName) ... "
         Write-Information "$indention Uploading $($pbix_file.FullName.Replace($root_path, '')) to $($workspace.Name)... "
